34e164793788528a3dc5e2b0199082bf1d48e310
allan bailey <allan@zirpu.org>
2019-08-28 08:39:12 -0700

    turned off delete as default.

--- bin/s3.backups.sh
+++ bin/s3.backups.sh
@@ -8,10 +8,16 @@ fi
 
 . $HOME/.virtualenvs/AWS3/bin/activate
 
+if [ "$1" = "delete" ]; then
+    delete="--delete"
+else
+    delete=
+fi
+
 # for S3 backups only backing up data dirs
 
 for d in Desktop Documents Music Pictures Videos
 do
-    aws s3 sync --acl private --delete ${d}/ s3://zirpus-backup/${d}/
+    aws s3 sync --acl private ${delete} ${d}/ s3://zirpus-backup/${d}/
 done
 
