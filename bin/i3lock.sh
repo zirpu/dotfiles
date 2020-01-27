2527d1636378eaed3a417494686d2003c2751fec
allan bailey <allan@zirpu.org>
2020-01-26 19:16:18 -0800

    getting i3 timeout autolock working.

--- bin/i3lock.sh
+++ bin/i3lock.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-i3lock -c 0f0f0f -e -f -p default 
+# add to .config/i3/config:
+#  exec_always xss-lock -- ~/bin/i3lock.sh
+
+
+revert(){
+    xset dpms 0 0 0
+    xset s off -dpms
+    xinput --set-prop $mouse "Device Enabled" "1"
+}
+
+trap revert HUP INT TERM
+
+xinput --set-prop $mouse "Device Enabled" "0"
+i3lock -t -f -n
+revert
+
+
+#i3lock -c 0f0f0f -e -f -p default
 #i3lock -c 0f0f0f -e -f -p default -i $HOME/Desktop/cogbias.jpg
 #i3lock -c 0f0f0f -e -f -p default -i /home/allan/Desktop/Images/cogbias.jpg
 
