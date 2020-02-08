6ed21073bce2bd62e3a9b4536deaa1d5843989f7
allan bailey <allan@zirpu.org>
2020-02-08 07:32:04 -0800

    fixing dpms settings.

--- bin/i3lock.sh
+++ bin/i3lock.sh
@@ -5,12 +5,14 @@
 
 
 revert(){
-    xset dpms 300 400 600
-    xset s +dpms
+    xset s -dpms
     xinput --set-prop $mouse "Device Enabled" "1"
 }
 
 trap revert HUP INT TERM
+
+xset s +dpms
+xset dpms 300 400 600
 
 xinput --set-prop $mouse "Device Enabled" "0"
 i3lock -t -f -n -c 0f0f0f
