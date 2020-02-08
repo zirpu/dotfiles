a7f68f75dc057afaff61fce69fe75f125b462958
allan bailey <allan@zirpu.org>
2020-02-08 12:19:28 -0800

    bah. screensaver foo.

--- bin/i3lock.sh
+++ bin/i3lock.sh
@@ -4,22 +4,27 @@
 #  exec_always xss-lock -- ~/bin/i3lock.sh
 
 
+log=$HOME/i3lock.log.$(date +%s)
+logit(){
+    echo "### $(date) ###" >> $log
+    xset q >> $log
+}
+
 revert(){
-    xset s -dpms
+#    xset -dpms
     xinput --set-prop $mouse "Device Enabled" "1"
 }
 
-trap revert HUP INT TERM
+trap revert HUP INT TERM EXIT
 
-xset s +dpms
+
+xset +dpms
 xset dpms 300 400 600
+#xset dpms 0 0 0
+#xset dpms force off
 
 xinput --set-prop $mouse "Device Enabled" "0"
-i3lock -t -f -n -c 0f0f0f
-revert
 
-
-#i3lock -c 0f0f0f -e -f -p default
-#i3lock -c 0f0f0f -e -f -p default -i $HOME/Desktop/cogbias.jpg
-#i3lock -c 0f0f0f -e -f -p default -i /home/allan/Desktop/Images/cogbias.jpg
-
+i3lock -t -f -n -c 000000
+#0f0f0f
+##
