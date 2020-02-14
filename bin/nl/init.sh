b4c6c9cff710c513da533ff4862a4fa221cdcf77
allan bailey <allan@zirpu.org>
2020-02-14 12:55:49 -0800

    need jq installed.

--- bin/nl/init.sh
+++ bin/nl/init.sh
@@ -2,7 +2,7 @@
 
 # Ubuntu 19.10, waiting for 20.04.
 if [ "$(lsb_release -is)" = "Ubuntu" ]; then
-   sudo apt install python3-dev python3-pip python3-virtualenvwrapper tmux
+   sudo apt install python3-dev python3-pip python3-virtualenvwrapper tmux jq
    pip3 install --user -r $HOME/bin/nl/reqs.txt
    # useful, but system package causes pip to refuse.
    pip3 install --user -U distro --force-reinstall
@@ -10,7 +10,7 @@ fi
 
 # Debian 10
 if [ "$(lsb_release -is)" = "Debian" ]; then
-   sudo apt install python3-dev python3-pip virtualenvwrapper tmux
+   sudo apt install python3-dev python3-pip virtualenvwrapper tmux jq
    pip3 install --user -r $HOME/bin/nl/reqs.txt
    # useful, but system package causes pip to refuse.
    pip3 install --user -U distro --force-reinstall
@@ -21,7 +21,7 @@ exit 0;
 # Fedora 31
 if [ -e /etc/os-release -a -e /etc/fedora-release ]; then
     if grep -q 'Fedora release 31' /etc/fedora-release; then
-       sudo dnf install python3-devel python3-virtualenvwrapper tmux
+       sudo dnf install python3-devel python3-virtualenvwrapper tmux jq
     fi
     pip3 install --user -r $HOME/bin/nl/reqs.txt
     # useful, but system package causes pip to refuse.
