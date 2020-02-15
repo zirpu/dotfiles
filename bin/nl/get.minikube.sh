0d22cd6843fcc435756659e5c73c79bf9b42947b
allan bailey <allan@zirpu.org>
2020-02-14 19:13:29 -0800

    adding get.minikube.sh util.

---
+++ bin/nl/get.minikube.sh
@@ -0,0 +1,8 @@
+
+
+# deb/ubu
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_1.7.2-0_amd64.deb \
+    && sudo apt install ./minikube_1.7.2-0_amd64.deb
+
+sudo apt install qemu-kvm libvirt-daemon
