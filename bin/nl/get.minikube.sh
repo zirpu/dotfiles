14ed97148141fdc81bbccf4e53a50c77e6d8e05c
allan bailey <allan@zirpu.org>
2020-02-15 15:33:33 -0800

    needed libvirt-system-daemon.

--- bin/nl/get.minikube.sh
+++ bin/nl/get.minikube.sh
@@ -5,5 +5,5 @@
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_1.7.2-0_amd64.deb \
     && sudo apt install ./minikube_1.7.2-0_amd64.deb
 
-sudo apt install qemu-kvm libvirt-daemon libvirt-clients
+sudo apt install qemu-kvm libvirt-daemon libvirt-clients libvirt-daemon-system
 
